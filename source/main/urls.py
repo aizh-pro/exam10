06b28b132f28651c3c736a7a5854816db8ed5403
Aitkeeva Aizhan <aaitkeeva@beeline.kz>
2021-01-23 12:16:59 +0600

    подключение accounts app

--- source/main/urls.py
+++ source/main/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from webapp import views as webapp_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    # path('', include('webapp.urls')),
+    path('', webapp_views.index_view)
 ]
